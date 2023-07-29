784d2051e769bbc4562f5b4dc3b5c99bce21cb0a
nickSwany <Thief.leroy@yandex.ru>
2023-07-29 19:32:40 +0300

    Commit

--- app/src/main/java/com/example/pl_market/MainActivity.kt
+++ app/src/main/java/com/example/pl_market/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.pl_market
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+
+        val searchButton = findViewById<Button>(R.id.search)
+        val mediatecaButton = findViewById<Button>(R.id.mediateka)
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+
+        mediatecaButton.setOnClickListener {
+            val mediatecaIntent = Intent(this, MediatecaActivity::class.java)
+            startActivity(mediatecaIntent)
+        }
+
+        settingsButton.setOnClickListener {
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
+
+
+
