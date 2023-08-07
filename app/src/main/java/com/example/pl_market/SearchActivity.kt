180cc6bd25e151a6e51afb1a588c5e06d9b2fd97
nickSwany <Thief.leroy@yandex.ru>
2023-08-07 20:33:54 +0300

    ДЗ 9 спринта

--- app/src/main/java/com/example/pl_market/SearchActivity.kt
+++ app/src/main/java/com/example/pl_market/SearchActivity.kt
@@ -14,12 +14,17 @@ import android.widget.ImageView
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var inputEditText: EditText
+
+    companion object {
+        var searchQuery = ""
+        const val SEARCH_STRING = "SEARCH_STRING"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val back_button = findViewById<Button>(R.id.back)
-         inputEditText = findViewById(R.id.search_edittext)
+        inputEditText = findViewById(R.id.search_edittext)
         val clearButton = findViewById<ImageView>(R.id.clean)
 
         back_button.setOnClickListener {
@@ -33,15 +38,14 @@ class SearchActivity : AppCompatActivity() {
 
         inputEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+                (s?.toString() ?: "").also { searchQuery = it }
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // empty
             }
         })
 
@@ -57,12 +61,14 @@ class SearchActivity : AppCompatActivity() {
         val textToSave = inputEditText.text.toString()
         outState.putString("searchText", textToSave)
     }
+
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
 
         val savedText = savedInstanceState.getString("searchText")
         inputEditText.setText(savedText)
     }
+
     private fun hideKeyboard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
--- app/src/main/java/com/example/pl_market/SettingsActivity.kt
+++ app/src/main/java/com/example/pl_market/SettingsActivity.kt
@@ -23,7 +23,7 @@ class SettingsActivity : AppCompatActivity() {
         shareView.setOnClickListener {
             shareCourseLink()
         }
-        supportView.setOnClickListener{
+        supportView.setOnClickListener {
             writeSupport()
         }
         userAgreement.setOnClickListener {
@@ -32,26 +32,26 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun shareCourseLink() {
-        val courseLink = "https://practicum.yandex.ru/profile/android-developer/"
+        val courseLink = getString(R.string.cours_uri)
         val sendIntent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, courseLink)
             type = "text/plan"
         }
-        startActivity(Intent.createChooser(sendIntent, "Поделиться ссылкой через:"))
+        startActivity(Intent.createChooser(sendIntent, getString(R.string.share_uri)))
     }
 
-    fun writeSupport(){
+    fun writeSupport() {
         val emailIntent = Intent(Intent.ACTION_SEND)
-        emailIntent.type="text/plan"
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("Thief.leroy@yandex.ru"))
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Сообщение разработчикам и разработчицам приложения Playlist Maker")
-        emailIntent.putExtra(Intent.EXTRA_TEXT, "Спасибо разработчикам и разработчицам за крутое приложение!")
-        startActivity((Intent.createChooser(emailIntent, "Отправить письмо через:")))
+        emailIntent.type = "text/plan"
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email)))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.inform))
+        emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.thanks))
+        startActivity((Intent.createChooser(emailIntent, getString(R.string.sent))))
     }
 
     fun openUserAgreement() {
-        val courseLinkUri = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+        val courseLinkUri = Uri.parse(getString(R.string.uri))
         val browserIntent = Intent(Intent.ACTION_VIEW, courseLinkUri)
         startActivity(browserIntent)
     }
